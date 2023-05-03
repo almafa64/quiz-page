66fa89474c4f11234ab750a1e9f99348af98f3f3
almafa64 <bhatalyak@gmail.com>
2023-05-04 00:28:27 +0200

    12.1 (started backend (queries and data get done), fixed bad css on a tag, moved repeated css codes to base.css)

--- oldal_inditasa.py
+++ oldal_inditasa.py
@@ -1,6 +1,7 @@
 import http.server
 import socketserver
 import webbrowser
+from urllib.parse import urlparse
 
 class m(http.server.SimpleHTTPRequestHandler):
 	def do_GET(self):
@@ -9,9 +10,15 @@ class m(http.server.SimpleHTTPRequestHandler):
 		return http.server.SimpleHTTPRequestHandler.do_GET(self)
 	def do_POST(self):
 		content_length = int(self.headers['Content-Length'])
-		post_data = self.rfile.read(content_length)
-		with open("scr\\files.js", "w") as f:
-			f.write("const data=" + post_data.decode('utf-8'))
+		post_data = self.rfile.read(content_length).decode('utf-8')
+		print(post_data)
+		queries = dict(x.split("=") for x in urlparse(self.path).query.split("&"))
+		quiz_type = queries["t"]
+		quiz_num = queries["q"]
+		topic_name = queries["n"]
+		print(queries)
+		#with open("scr\\files.js", "w") as f:
+		#	f.write("const data=" + post_data)
 		self.send_response(200)
 		self.end_headers()
 
--- scr/maker.js
+++ scr/maker.js
@@ -1,19 +1,18 @@
 const type = document.getElementById("type");
 const hold = document.getElementById("hold");
+const saveBut = document.getElementById("save");
 
 var quizType = NaN;
+var topicName = "minecraft";
+var quizNum = 0;
 
 function addRow(){
-	if(isNaN(quizType)) return;
+	if(isNaN(quizType)) return false;
 
 	const p = hold.appendChild(document.createElement("p"));
 	p.append(`${hold.childElementCount}, `, document.createElement("textarea"));
 
 	switch(quizType){
-		case 0:
-		case 1:
-		case 3: // sorba + jelölő + választó
-			break;
 		case 2: // párba
 			p.append(" - ", document.createElement("textarea"));
 			break;
@@ -32,10 +31,37 @@ function addRow(){
 		addRow.button = button;
 	}
 	else p.appendChild(addRow.button);
+	return true;
 }
 
 function changeType(){
 	quizType = parseInt(type.value);
 	hold.innerHTML = "";
-	addRow();
+	saveBut.disabled = !addRow();
+}
+
+function save(){
+	var send = "";
+	const areas = hold.querySelectorAll("textarea");
+
+	areas.forEach(e => {
+		if(e.value.length != 0) send += e.value + ",";
+	});
+	send = send.slice(0, -1);
+	
+	fetch(`/save?t=${quizType}&n=${topicName}&q=${quizNum}`, {
+		method: 'POST',
+		headers: {
+			'Accept': 'text/plain',
+			'Content-Type': 'text/plain'
+		},
+		body: send
+	})
+	//.then(() => window.location.reload())
+	.catch(err => console.error(err));
+}
+
+if(location.protocol == "file:"){
+	alert("Ez az oldal nem elérhető szerver nélkül (és valószínűleg egy lusta programozó miatt tudtad elérni)");
+	window.history.back();
 }
